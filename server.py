cef45d2b672162b6cf5f49f698e7ef331ce32fd9
rin4th <rizalnur.art@gmail.com>
2023-05-04 09:30:50 +0700

    first run success

--- server.py
+++ server.py
@@ -4,19 +4,25 @@ def threading_socket(connectionSocket):
     try:
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
-        header = "HTTP/1.1 200 OK\nCOntent-Type: text/html\r\n\r\n"
-        if filename[1:] == "/" or filename[1:] == "/index.html":
-            message_body = open("index.html", 'r')
-        connectionSocket.send(header.encode())
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
+        response = handle_request(filename)
+        connectionSocket.send(response.encode())
         connectionSocket.close()
     except IOError:
         connectionSocket.send("File Not Found".encode())
         connectionSocket.close()
+
+def handle_request(filename):
+    response_line = "HTTP/1.1 200 OK\r\n"
+    content_type = "Content-Type: text/html\r\n\r\n"
+    print(filename[0])
+    if filename[0] == "/" or filename[0] == "/index.html":
+        file = open("index.html", 'r')
+    else:
+        file = open("404.html", 'r')
+    message_body = file.read()
+    file.close()
+    response = response_line+content_type+message_body
+    return response
 
 def handle_method(method):
     if method == "GET":
