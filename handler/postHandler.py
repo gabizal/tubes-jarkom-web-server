6adfe5c0a7f12ea2180d279876684ff04a8f6ab3
Rizal Nur <52568308+rin4th@users.noreply.github.com>
2023-05-05 12:07:21 +0700

    Fix directory database

--- handler/postHandler.py
+++ handler/postHandler.py
@@ -4,7 +4,7 @@ from messageHandler import *
 def handlePOST(request):
     search = request.split("\n")[25].replace("\r", "")
     result = []
-    files = os.listdir('./database')
+    files = os.listdir('../database')
     for name in files:
         if search in name:
             result.append(name)
